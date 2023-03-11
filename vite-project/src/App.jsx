9ed3adc68bf2f2d80df4cbf323fd31c382909d69
Tesnime <tesnimebaifouh@gmail.com>
2023-03-11 14:02:30 +0100

    react hook project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,60 @@
+import React from "react";
 import "./App.css";
-import Profile from "./components/Profile/Profile.jsx";
-import image from "./assets/profile.jpg";
 
-function App() {
-  const handleName = (fullName) => {
-    alert(`Name: ${fullName}`);
-  };
-  return (
-    <div className="App">
-      <Profile
-        fullName="tesnime"
-        bio="I'm a web developer."
-        profession="Web Developer"
-        handleName={handleName}
-      />
-      <img src={image} alt="Profile" />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const person = {
+      fullName: "tesnime",
+      bio: "i dont have one yet .",
+      imgSrc: "https://via.placeholder.com/150",
+      profession: "Dev",
+    };
+
+    this.state = {
+      person: person,
+      show: false,
+      timeInterval: 0,
+    };
+
+    this.toggleShow = () => {
+      this.setState((prevState) => ({ show: !prevState.show }));
+    };
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(() => {
+      this.setState((prevState) => ({
+        timeInterval: prevState.timeInterval + 1,
+      }));
+    }, 1000);
+
+    this.intervalId = intervalId;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    const { person, show, timeInterval } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>Toggle Show</button>
+        {show && (
+          <div>
+            <img src={person.imgSrc} alt={person.fullName} />
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <p>{person.profession}</p>
+          </div>
+        )}
+        <p>Time Interval: {timeInterval} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
